951ffc84ea3185cbd3f9742c964bb9501792cb17
Lucas de Oliveira - Catstein <lucasdeoliveira@pecege.com>
2025-10-20 20:05:51 -0300

    adicionando componente de botão do shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
-import { Button } from "@/components/Button";
-
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center gap-1.5">
-      <Button
-        onClick={() => {
-          alert("OPA, CLICOU AI CARA");
-        }}
-        text="clica ai irmao"
-      ></Button>
+      HELLO WORLD
     </div>
   );
 }
