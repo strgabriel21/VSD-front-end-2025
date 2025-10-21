9b4d4ff76e900a206e66c811f3e340eb86a1a55b
Your Name <gabriel-reis-dionisio@hotmail.com>
2025-10-21 20:20:36 -0300

    subindo

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LoginPage() {
+  return (
+    <section className="w-[90%] h-[90%] bg-gray-200 rounded-2xl shadow-xs shadow-gray-400 flex items-center justify-center">
+      <div className="w [34.375rem] h [43.75rem] bg-amber-600"></div>
+    </section>
+  );
+}
